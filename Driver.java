de3a58cbb22f0129828467034f505c5105fa817c
Fedor Menchukov <77518360+menchukf@users.noreply.github.com>
2025-09-24 12:28:24 +0000

    setup

---
+++ Driver.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Driver{
+public static void main(String[] args){
+    Scanner scan  = new Scanner(System.in);
+    Soup mySoup = new Soup();
+    while(true){
+       String input = scan.nextLine(); 
+        if(input.contains("add")){
+            mySoup.add(input.replace("add ",""));
+            System.out.println("new letters is "+mySoup.getLetters());
+        }
+        else if(input.contains("randomLetter")){
+            System.out.println(mySoup.randomLetter());
+        }
+        else if(input.contains("centered")){
+              System.out.println(mySoup.companyCentered());
+        }
+          else if(input.contains("company")){
+             mySoup.setCompany(input.replace("company ", ""));
+        }
+          else if(input.contains("removeVowel")){
+             mySoup.removeFirstVowel();
+             System.out.println("new letters is "+mySoup.getLetters());
+        }
+        else if(input.contains("removeSome")){
+             mySoup.removeSome(Integer.parseInt(input.replace("removeSome ", "")));
+             System.out.println("new letters is "+mySoup.getLetters());
+        }
+         else if(input.contains("removeWord")){
+             mySoup.removeWord(input.replace("removeWord ", ""));
+             System.out.println("new letters is "+mySoup.getLetters());
+        }
+    }
+
+}
+
+
+
+}
---
+++ Soup.java
@@ -0,0 +1,65 @@
+public class Soup {
+    //these are instance variables 
+    private String letters;
+    private String company;
+    private int wordCount;
+
+    //this is a constructor it sets the instance variables (more on this later in the year)
+    public Soup(){
+        letters ="";
+        company = "none";
+        wordCount = 0;
+    }
+
+
+    //sets the name of the company to the provided name
+    public void setCompany(String company){
+        this.company = company;
+    }
+
+    //returns the company name
+    public String getCompany(){
+        return company;
+    }
+
+    //returns letters
+    public String getLetters(){
+        return letters;
+    }
+
+//below are the functions you'll be writing.
+
+    //adds a word to the pool of letters known as "letters"
+    public void add(String word){
+
+    }
+
+
+    //Use Math.random() to get a random character from the letters string and return it.
+    public char randomLetter(){
+        return 'a';
+    }
+
+
+    //returns the letters currently stored with the company name placed directly in the center of all
+    //the letters
+    public String companyCentered(){
+        return "";
+    }
+
+
+    //should remove the first availabile vowel from letters. If there are no vowels this method has no effect.
+    public void removeFirstVowel(){
+        
+    }
+
+    //should remove "num" letters from a random spot in the string letters. You may assume num never exceeds the length of the string.
+    public void removeSome(int num){
+
+    }
+
+    //should remove the word "word" from the string letters. If the word is not found in letters then it does nothing.
+    public void removeWord(String word){
+        
+    }
+}
